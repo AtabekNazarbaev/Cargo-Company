b67f9217797063bc8a6dd4e046bb2afd2c748522
Cargo Company <atabeknazarbaev@gmail.com>
2026-02-16 18:22:15 +0300

    first commit

--- app/src/main/java/com/example/laba1/helper/DiscountCalculator.kt
+++
@@ -1,8 +0,0 @@
-//package com.example.laba1.helper
-//
-//class DiscountCalculator(private var strategy: DiscountStrategy) {
-//
-//    fun calculate(amount: Double, discount: Int): Double {
-//        return strategy.calculate(amount, discount)
-//    }
-//}
--- app/src/main/java/com/example/laba1/helper/FixedDiscount.kt
+++
@@ -1,9 +0,0 @@
-//package com.example.laba1.helper
-//
-//class FixedDiscount : DiscountStrategy {
-//
-//    override fun calculate(discount: Double, amount: Int): Double {
-//        return amount.toDouble()
-//    }
-//
-//}
